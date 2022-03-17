49d02cf475341cb02bd27f75706917f91a442852
ohorikazuya <ohorikazuya@team-lab.com>
2022-03-17 16:43:54 +0900

    first commit

---
+++ UNDERSTANDING-TS/app.js
@@ -0,0 +1,10 @@
+var person = {
+    name: 'jhon',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}

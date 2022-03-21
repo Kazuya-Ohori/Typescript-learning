38dcaf04d73c7bba4f80573f289a495b647b100f
ohorikazuya <ohorikazuya@team-lab.com>
2022-03-21 14:59:17 +0900

    Destructuring of Array&Object

--- MODERN-TS/src/app.ts
+++ MODERN-TS/src/app.ts
@@ -2,13 +2,13 @@
 //   return a + b;
 // };
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-const printOutput: (output: string | number) => void = (output) => {
-  console.log(output);
-};
+// const printOutput: (output: string | number) => void = (output) => {
+//   console.log(output);
+// };
 
-printOutput(add(20));
+// printOutput(add(20));
 
 const button = document.querySelector("button");
 
@@ -26,7 +26,7 @@ const activeHobbies = ["Hiking", ...hobbies];
 activeHobbies.push(...hobbies); // →でも書ける　const activeHobbies = ["Hiking", ...hobbies];
 
 const person = {
-  name: "Max",
+  firstName: "Max",
   age: 30,
 };
 
@@ -35,3 +35,28 @@ const copiedPerson = {
 };
 
 console.log(copiedPerson);
+
+// Rest Parameter
+const add = (...numbers: number[]) => {
+  let result = 0;
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+
+console.log(addedNumbers);
+
+// Destructuring = 分割代入
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+// 配列の分割代入
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+// オブジェクトの分割代入
+const { firstName, age } = person;
+console.log(firstName, age);
+const { firstName: userName, age: userAge } = person;
+console.log(userName, userAge);
